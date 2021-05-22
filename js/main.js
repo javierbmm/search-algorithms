2b6b92c4d8cce985edd84e1df7d261b67b4ec5ac
Barbara <barbara.rekowska@magasin.dk>
2021-05-22 13:45:02 +0200

    Initial structure of the code, being able to set the from and to city, firing a_search function on search click

--- js/main.js
+++ js/main.js
@@ -1,4 +1,84 @@
 import * as data from './nodes.js'
+import { findASearchPath } from './a_search.js'
 
-console.log(data.cities);
-console.log(data.connections);
+loadCities();
+addActiveClass();
+
+function loadCities() {
+    //Setting the city buttons to be able to choose
+    //the starting point and the destination
+    for(let i=0; i< data.cities.length; i++)
+    {
+        let city = data.cities[i];
+        let cityButton = document.createElement("button");
+        let cityButtonText;
+        cityButton.value = city.name;
+        cityButton.name = city.name;
+        if(i==0) {
+            cityButton.className = "city-btn city-btn-active";
+        }else{
+            cityButton.className = "city-btn";
+        }
+        cityButtonText = document.createTextNode(city.name);
+        cityButton.appendChild(cityButtonText)
+        document.getElementById("from-city").appendChild(cityButton);
+
+        let cityButton2 = document.createElement("button");
+        let cityButtonText2;
+        cityButton2.value = city.name;
+        cityButton2.name = city.name;
+        if(i==0) {
+            cityButton2.className = "city-btn city-btn-active";
+        }else{
+            cityButton2.className = "city-btn";
+        }
+        cityButtonText2 = document.createTextNode(city.name);
+        cityButton2.appendChild(cityButtonText2)
+
+        document.getElementById("to-city").appendChild(cityButton2);
+    }
+}
+
+function addActiveClass(){
+    let header = document.getElementById("from-city");
+    let buttons = header.getElementsByClassName("city-btn");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+            let current = header.getElementsByClassName("city-btn-active");
+            current[0].className = current[0].className.replace(" city-btn-active", "");
+            this.className += " city-btn-active";
+        });
+    }
+
+    let header2 = document.getElementById("to-city");
+    let buttons2 = header2.getElementsByClassName("city-btn");
+    for (let i = 0; i < buttons2.length; i++) {
+        buttons2[i].addEventListener("click", function() {
+            let current2 = header2.getElementsByClassName("city-btn-active");
+            current2[0].className = current2[0].className.replace(" city-btn-active", "");
+            this.className += " city-btn-active";
+        });
+    }
+
+    //Setting the search-btn event listener
+    document.getElementById("search-btn").addEventListener("click", findPath);
+}
+
+function findPath(){
+    //Firstly check if the cities are the same
+    let fromCityHeader = document.getElementById("from-city");
+    let fromCity = fromCityHeader.getElementsByClassName("city-btn city-btn-active")[0];
+    console.log(fromCity)
+    let toCityHeader = document.getElementById("to-city");
+    let toCity = toCityHeader.getElementsByClassName("city-btn city-btn-active")[0];
+    console.log(toCity);
+
+    let header = document.getElementById("algorithm-options");
+    let algorithms = header.getElementsByClassName("algorithm-btn");
+    for(let i = 0; i < algorithms.length; i++){
+        if(algorithms[i].id == "a_search"){
+            findASearchPath(fromCity.value, toCity.value);
+        }
+    }
+}
+
